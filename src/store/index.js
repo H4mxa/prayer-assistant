c4306a3aa4bed9b1dc461fa029c59c549d90d19b
H4mxa <duke80800@gmail.com>
2021-03-08 12:01:36 +0000

    addLoggerToDispatch

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,25 @@
 import { createStore, combineReducers } from 'redux';
 import servicesReducer from 'reducers';
+
+const addLoggerToDispatch = (store) => {
+  const dispatch = store.dispatch;
+  /*
+    we are returning here function which will take some action. 
+    When we providing dispatch function to our store dispatch we need to follow exact same structure of orignal dispatch
+    function. Orignal dispatch function take action and dispatch function to reducer. 
+  */
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = dispatch(action);
+    // Now as we dispatch action action, our store should be updated. dispatch will called reducer and it should update our state
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 /* You need to remember few things first in redux.
     Actions | Action creators
     dispatch
@@ -27,6 +47,8 @@ const initStore = () => {
     serviceApp,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+  // overwriting dispatch function
+  store.dispatch = addLoggerToDispatch(store);
   return store;
 };
 

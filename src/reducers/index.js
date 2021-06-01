a3e23ff57f978af5b8f92f868ebbd92c26cddf2e
h4mxa <duke80800@gmail.com>
2021-06-01 01:30:43 -0700

    subscribe to collaborations

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,14 @@ import selectedService from "./selectedService";
 import auth from "./auth";
 import offers from "./offers";
 
+import collaboration from "./collaboration";
+
 const serviceApp = combineReducers({
   services,
   selectedService,
   auth,
   offers,
+  collaboration,
 });
 
 export const getMessages = (state) => state.auth.user.messages;
